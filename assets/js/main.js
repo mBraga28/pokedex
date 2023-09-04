9721b988711246154fb1fe31cc53513f0042c026
Marco Braga <maboa17@gmail.com>
2023-09-04 18:16:53 -0300

    feat: update Pokemon List
    Turning a Pokémon List into an HTML List

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,40 @@
 
-console.log('Hello World!!!');
+
+const offset = 0;
+const limit = 10;
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+function convertPokemonToLi(pkmn) {
+    return `
+                <li class="pkmn">
+                    <span class="number">#001</span>
+                    <span class="name">${pkmn.name}</span>
+
+                    <div class="detail">
+                        <ol class="types">
+                            <li class="type">grass</li>
+                            <li class="type">poison</li>
+                        </ol>
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pkmn.name}">
+                    </div>
+                </li>
+            `
+}
+
+const pokemonList = document.getElementById('pokemonList');
+
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemon) => {
+        
+        for (let i = 0; i < pokemon.length; i++) {
+            const pkmn = pokemon[i];
+            pokemonList.innerHTML += convertPokemonToLi(pkmn);
+        }
+
+    })
+    .catch((error) => console.error(error));
+
