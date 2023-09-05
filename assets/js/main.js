3efcb4c3b33c2c86891f6b0f11dd7967e16b56e8
Marco Braga <maboa17@gmail.com>
2023-09-05 13:27:50 -0300

    feat: add poke-api.js; refactor: update main.js
    Separating API consumption from HTML manipulation and utilizing the Map function to reduce code verbosity

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,5 @@
 
 
-const offset = 0;
-const limit = 10;
-
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
 function convertPokemonToLi(pkmn) {
     return `
                 <li class="pkmn">
@@ -25,16 +20,7 @@ function convertPokemonToLi(pkmn) {
 const pokemonList = document.getElementById('pokemonList');
 
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemon) => {
-        
-        for (let i = 0; i < pokemon.length; i++) {
-            const pkmn = pokemon[i];
-            pokemonList.innerHTML += convertPokemonToLi(pkmn);
-        }
-
-    })
-    .catch((error) => console.error(error));
+pokeApi.getPokemon().then((pokemon = []) => {
+    pokemonList.innerHTML += pokemon.map(convertPokemonToLi).join('')
+})
 
